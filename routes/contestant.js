2d2af97d570754e7142a023e1d415918290e21c0
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-08-28 12:29:44 +0530

    Update contestant.js

--- routes/contestant.js
+++ routes/contestant.js
@@ -1,15 +1,18 @@
 const express=require("express");
 const router=express.Router();
-const {createContestant, increVotes, getContestantById,getContestantIdByName,getAllContestants}=require("../controllers/contestant")
+const {createContestant, increVotes, getContestantById,getContestantIdByName,getAllContestants,getVoteCount}=require("../controllers/contestant")
 const {isSignedIn,isAuthenticated,isAdmin}=require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
 router.param("contestantId",getContestantById)
 router.param("userId",getUserById)
+router.param("numberOfVotes",getVoteCount)
 
 router.post("/user/:userId/contestant/create",isSignedIn,isAuthenticated,isAdmin,createContestant)
 
-router.get("/user/:userId/contestant/incre/:contestantId",isSignedIn,isAuthenticated,increVotes)
+router.get("/user/:userId/contestant/incre/:contestantId/:numberOfVotes",isSignedIn,isAuthenticated,increVotes)
+// router.get("/user/:userId/contestant/incre/:contestantId/:numberOfVotes",increVotes)
+
 router.post("/user/:userId/nominations/contestant",isSignedIn,isAuthenticated,getContestantIdByName)
 router.get("/all/contestants",getAllContestants)
 
